d495ce3a3951a4ec93e6fb2a59c78fdf4f6aa21f
wangsq <358009775@qq.com>
2020-03-16 23:45:39 +0800

    引入styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Globalstyle } from './style';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Globalstyle />
         Learn React
       </div>
     )
