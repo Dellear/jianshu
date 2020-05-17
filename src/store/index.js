0cbce072b4b313d0d4187e26867970b0c93e3c53
wang <358009775@qq.com>
2020-05-17 11:22:42 +0800

    Header组件改成无状态组件

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,12 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
 
-const store = createStore(reducer);
+const devToolState = (() => {
+  if (process.env.NODE_ENV !== 'production') {
+    return window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+  }
+})();
+
+const store = createStore(reducer, devToolState);
 
 export default store;
